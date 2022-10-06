1d12b0f4d6b7e6ff78ac149b8a401c9cbd143d67
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-06 19:52:00 +0200

    next & prev logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import data from "./data";
+
+import React from 'react';
+
 import Review from "./Review";
 
+
 function App() {
+  const [imageId, setImageId ] = React.useState(1);
+
+  function nextImage() {
+    if(imageId === 4) {
+      setImageId(0)
+    }
+    setImageId(prevState => prevState + 1);
+  }
+
+  function prevImage() {
+    if(imageId === 1) {
+      setImageId(5);
+    } 
+    setImageId(prevState => prevState - 1);
+
+  }
   const reviewElement = data.map((review) => {
-    return < Review key={review.id} name={review.name} job={review.job} img={review.image} text={review.text} />
+    return < Review key={review.id} name={review.name} job={review.job} img={review.image}
+     text={review.text}  id={review.id} imageId={imageId} nextImage={nextImage} prevImage={prevImage}/>
   })
   return (
     <main>
--- src/Review.jsx
+++ src/Review.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 
 
-export default function Review({name,job,img,text}) {
+export default function Review({name,job,img,text, id, imageId, nextImage, prevImage}) {
 
   return (
-    <div className="review">
-      <div className="img-container">
-        <img src={img} alt={name} className="person-img" />
-        <span className="quote-icon">
-        <i className="fa-solid fa-quote-right"></i>
-        </span>
-      </div>
-      <h4 className="author">{name}</h4>
-      <p className="job">{job}</p>
-      <p className="info">{text}</p>
-      <button  className="prev-btn">
-        <span><i className="fa-solid fa-chevron-left"></i></span>
-      </button>
-      <button className="next-btn">
-      <span><i className="fa-solid fa-chevron-right"></i></span>
-      </button>
+    id === imageId ?  <div className="review">
+    <div className="img-container">
+      <img src={img} alt={name} className="person-img" />
+      <span className="quote-icon">
+      <i className="fa-solid fa-quote-right"></i>
+      </span>
     </div>
+    <h4 className="author">{name}</h4>
+    <p className="job">{job}</p>
+    <p className="info">{text}</p>
+    <button onClick={() => prevImage()} className="prev-btn">
+      <span><i className="fa-solid fa-chevron-left"></i></span>
+    </button>
+    <button onClick={() => nextImage()} className="next-btn">
+    <span><i className="fa-solid fa-chevron-right"></i></span>
+    </button>
+  </div> : ""
+   
   )
 }
